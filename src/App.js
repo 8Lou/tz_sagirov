4f8e3f4c8219868b8fe2cbb48422a7916ddeb883
8Lou <lou8@mail.ru>
2024-01-11 15:17:52 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import Main from './components/Main';
 
 const App = () => {
   return (
      <div className="">
       <Header />
+      <Main />
       
     </div>
   );
